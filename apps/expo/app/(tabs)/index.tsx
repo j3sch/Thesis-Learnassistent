7a22c7371fcfeabe644c40473ccc37aa7d370a59
Jens Schlegel <39447994+j3sch@users.noreply.github.com>
2024-05-03 13:32:14 +0200

    vite mono repo

--- apps/expo/app/(tabs)/index.tsx
+++ apps/expo/app/(tabs)/index.tsx
@@ -1,31 +1,31 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
-import { trpc } from '@/utils/trpc'
-import { Paragraph, YStack } from 'tamagui'
+import { trpc } from "@/utils/trpc";
+import { Paragraph, YStack } from "tamagui";
 
 export default function TabOneScreen() {
-    const { data } = trpc.hello.world.useQuery<string>('world')
+	const { data } = trpc.hello.world.useQuery<string>("world");
 
-    return (
-        <YStack backgroundColor={'$background'} f={1} justifyContent='center'>
-            <Paragraph>{data}</Paragraph>
-        </YStack>
-    )
+	return (
+		<YStack backgroundColor={"$background"} f={1} justifyContent="center">
+			<Paragraph>{data}</Paragraph>
+		</YStack>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
-})
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
+});
