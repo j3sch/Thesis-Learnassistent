e9781190574b2d64827843da459fed315cc1bdb8
Jens Schlegel <39447994+j3sch@users.noreply.github.com>
2024-05-03 14:26:46 +0200

    remove expo app

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import tamaguiConfig from './src/tamagui.config.ts'
+import { tamaguiExtractPlugin, tamaguiPlugin } from '@tamagui/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
-});
+    plugins: [
+        react(),
+        TanStackRouterVite(),
+        tamaguiPlugin(tamaguiConfig),
+        process.env.NODE_ENV === 'production' ? tamaguiExtractPlugin(tamaguiConfig) : null,
+    ].filter(Boolean),
+})
