e567656a1e44d1d952a10fd1094ca73c419cd1ba
Jens Schlegel <39447994+j3sch@users.noreply.github.com>
2024-05-24 22:01:59 +0200

    remove tsc from build script

---
+++ packages/api/.wrangler faser und garne/tmp/bundle-1X6sHf/middleware-insertion-facade.js
@@ -0,0 +1,24 @@
+import worker, * as OTHER_EXPORTS from '/Users/jensschlegel/Documents/coding/Thesis-Learnassistent/packages/api/src/worker.ts'
+import * as __MIDDLEWARE_0__ from '/Users/jensschlegel/Documents/coding/Thesis-Learnassistent/packages/api/node_modules/.pnpm/wrangler@3.22.3/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts'
+const envWrappers = [__MIDDLEWARE_0__.wrap].filter(Boolean)
+const facade = {
+  ...worker,
+  envWrappers,
+  middleware: [__MIDDLEWARE_0__.default, ...(worker.middleware ? worker.middleware : [])].filter(
+    Boolean
+  ),
+}
+export * from '/Users/jensschlegel/Documents/coding/Thesis-Learnassistent/packages/api/src/worker.ts'
+
+const maskDurableObjectDefinition = (cls) =>
+  class extends cls {
+    constructor(state, env) {
+      let wrappedEnv = env
+      for (const wrapFn of envWrappers) {
+        wrappedEnv = wrapFn(wrappedEnv)
+      }
+      super(state, wrappedEnv)
+    }
+  }
+
+export default facade
